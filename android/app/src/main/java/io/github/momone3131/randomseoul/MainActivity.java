58da93173986c4227242968bd97697a676ea11a0
momone3131 <100558052+momone3131@users.noreply.github.com>
2026-09-12 14:19:35 +0900

    android: register RandomSeoulPlaces native plugin

--- android/app/src/main/java/io/github/momone3131/randomseoul/MainActivity.java
+++ android/app/src/main/java/io/github/momone3131/randomseoul/MainActivity.java
@@ -1,5 +1,13 @@
 package io.github.momone3131.randomseoul;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(RandomSeoulPlacesPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
